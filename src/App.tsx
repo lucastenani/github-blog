8fc3e9645ce6808725c66d2e28903a9bbcd50741
lucastenani <lucas.tenani3@gmail.com>
2024-08-01 16:32:44 -0300

    style - Configure font and theme in Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import '@/globals.css'
+
 export function App() {
-  return <h1>Hello, World!</h1>
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1>Hello, World!</h1>
+    </div>
+  )
 }
