97eb7fff5c1af8fd385ed6e194b97536e6a83a1e
lucastenani <lucas.tenani3@gmail.com>
2024-08-01 16:43:45 -0300

    feat - Create layout structure and pattern for main pages with react-router-dom

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
 import { Home } from './pages/home'
 import { Post } from './pages/post'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/post/:id',
-    element: <Post />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/post/:id',
+        element: <Post />,
+      },
+    ],
   },
 ])
