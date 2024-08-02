dced94bd6d40f7acc5db16895594a0bcb833dcbc
lucastenani <lucas.tenani3@gmail.com>
2024-08-02 16:16:23 -0300

    feat - Add Query Client Provider to app for global access to requests and caching

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import '@/globals.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | GitHub Blog" />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </HelmetProvider>
   )
 }
