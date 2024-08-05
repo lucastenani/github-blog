50d02de8a62d605ef45162acdc11cb1d99e6fb06
lucastenani <lucas.tenani3@gmail.com>
2024-08-05 15:55:02 -0300

    feat - Create skeletons for loading states during requests

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,134 @@
+import {
+  GithubLogo,
+  Link as LinkIcon,
+  MapPin,
+  Users,
+} from '@phosphor-icons/react'
+import { useQuery } from '@tanstack/react-query'
+import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
+
+import { getIssues } from '@/api/get-issues'
+import { getUser } from '@/api/get-user'
+import { Pagination } from '@/components/pagination'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+
+import { HomeSkeleton } from './skeleton'
+
+export function Home() {
+  const { data: profile, isFetched: isFetchedProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getUser,
+  })
+
+  const { data: issues, isFetched: isFetchedIssues } = useQuery({
+    queryKey: ['issues'],
+    queryFn: getIssues,
+  })
+
+  function concatString(text: string, textType: 'title' | 'body'): string {
+    if (textType === 'title') {
+      return text.length > 30 ? text.substring(0, 30).concat('...') : text
+    }
+
+    return text.length > 160 ? text.substring(0, 160).concat('...') : text
+  }
+
+  return (
+    <>
+      <Helmet title="Home" />
+
+      {isFetchedIssues && isFetchedProfile ? (
+        <div className="space-y-10">
+          <section>
+            <Card className="flex flex-col md:flex-row md:items-center md:p-4">
+              <img
+                className="w-full rounded-t-lg md:h-48 md:max-w-48 md:rounded-lg"
+                src={profile?.avatar_url}
+                alt={`${profile?.login} profile avatar`}
+              />
+              <div className="md:flex-1">
+                <CardHeader className="md:flex-row md:flex-wrap md:items-center md:justify-between">
+                  <CardTitle>{profile?.name}</CardTitle>
+                  <CardDescription>
+                    <a
+                      className="flex cursor-pointer items-center gap-1 font-semibold uppercase text-primary hover:underline hover:opacity-75"
+                      href={`https://github.com/${profile?.login}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      github
+                      <LinkIcon size={20} />
+                    </a>
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-muted-foreground">{profile?.bio}</p>
+                </CardContent>
+                <CardFooter className="flex flex-col items-start md:flex-row md:flex-wrap md:gap-4">
+                  <div className="flex items-center gap-2">
+                    <GithubLogo size={20} className="text-muted-foreground" />
+                    {profile?.login}
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <MapPin size={20} className="text-muted-foreground" />
+                    {profile?.location}
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Users size={20} className="text-muted-foreground" />
+                    {profile?.followers} followers
+                  </div>
+                </CardFooter>
+              </div>
+            </Card>
+          </section>
+
+          <section>
+            <div className="mb-4 md:flex md:items-center md:justify-between">
+              <h2 className="text-2xl">Publications</h2>
+              <span className="text-sm text-muted-foreground">
+                {issues?.total_count} publications
+              </span>
+            </div>
+
+            <Input
+              placeholder="Search content"
+              className="focus:border-primary"
+            />
+          </section>
+
+          <section className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            {issues?.items.map((issue) => {
+              return (
+                <Link key={issue.id} to={`/post/${issue.number}`}>
+                  <Card className="hover:opacity-85 md:h-[265px]">
+                    <CardHeader className="md:flex md:flex-row md:flex-wrap md:justify-between md:gap-2">
+                      <CardTitle className="text-xl md:max-w-xs">
+                        {concatString(issue.title, 'title')}
+                      </CardTitle>
+                      <CardDescription>{issue.created_at}</CardDescription>
+                    </CardHeader>
+                    <CardContent className="break-words text-muted-foreground">
+                      {concatString(issue.body, 'body')}
+                    </CardContent>
+                  </Card>
+                </Link>
+              )
+            })}
+          </section>
+          <Pagination pageIndex={0} perPage={10} totalCount={105} />
+        </div>
+      ) : (
+        <HomeSkeleton />
+      )}
+    </>
+  )
+}
---
+++ src/pages/post/skeleton.tsx
@@ -0,0 +1,68 @@
+import {
+  CalendarDots,
+  CaretLeft,
+  ChatCircle,
+  GithubLogo,
+  Link as LinkIcon,
+} from '@phosphor-icons/react'
+import { Link } from 'react-router-dom'
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+
+export function PostSkeleton() {
+  return (
+    <div className="space-y-10">
+      <section>
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-between gap-1">
+              <Link
+                to={'/'}
+                className="flex cursor-pointer items-center gap-1 font-semibold uppercase text-primary hover:underline hover:opacity-75"
+              >
+                <CaretLeft size={18} />
+                return
+              </Link>
+              <p className="flex cursor-not-allowed items-center gap-1 font-semibold uppercase text-primary">
+                see on github
+                <LinkIcon size={18} />
+              </p>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <CardTitle className="text-3xl">
+              <Skeleton className="h-[40px] w-4/5 rounded-full" />
+            </CardTitle>
+          </CardContent>
+          <CardFooter className="flex flex-col items-start gap-2 md:flex-row md:flex-wrap md:gap-4">
+            <div className="flex items-center gap-2">
+              <GithubLogo size={20} className="text-muted-foreground" />
+              <Skeleton className="h-[20px] w-[100px] rounded-full" />
+            </div>
+            <div className="flex items-center gap-2">
+              <CalendarDots size={20} className="text-muted-foreground" />
+              <Skeleton className="h-[20px] w-[100px] rounded-full" />
+            </div>
+            <div className="flex items-center gap-2">
+              <ChatCircle size={20} className="text-muted-foreground" />
+              <Skeleton className="h-[20px] w-[100px] rounded-full" />
+            </div>
+          </CardFooter>
+        </Card>
+      </section>
+
+      <section className="space-y-3">
+        {Array.from({ length: 8 }).map((_, i) => {
+          return <Skeleton key={i} className="h-[20px] w-full rounded-full" />
+        })}
+      </section>
+    </div>
+  )
+}
