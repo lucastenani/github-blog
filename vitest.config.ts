378ffe2dfa0313107cd9421b80852984d58c5089
lucastenani <lucas.tenani3@gmail.com>
2024-08-08 14:49:47 -0300

    build - Install and configure Testing Library and Happy DOM

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
   },
-})
+} as UserConfig)
