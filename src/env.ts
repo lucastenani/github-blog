4f1af0e813974a045b36a8657e38ea8cdf17e53c
lucastenani <lucas.tenani3@gmail.com>
2024-08-09 17:26:08 -0300

    test - Set up MSW across application in test environment

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,12 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  MODE: z.enum(['production', 'development', 'test']).default('development'),
   VITE_API_URL: z.string().url(),
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
-  VITE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  VITE_ENV: z
+    .enum(['production', 'development', 'test'])
+    .default('development'),
 })
 
 export const env = envSchema.parse(import.meta.env)
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,7 +6,7 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
 })
 
-if (env.VITE_ENABLE_API_DELAY && env.VITE_ENV === 'dev') {
+if (env.VITE_ENABLE_API_DELAY && env.VITE_ENV === 'development') {
   api.interceptors.request.use(async (config) => {
     await new Promise((resolve) =>
       setTimeout(resolve, Math.round(Math.random() * 3000)),
